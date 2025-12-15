7b9b16b24e1b8f1573cff6ff55dbc97bc954e5ad
Bruno_Ss <brunosch250@gmail.com>
2025-12-15 17:02:27 -0300

    Implemented Player anims and Score/Combo Canva

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
 
     private Vector3 currentScale;//Essa variavel guarda a escala atual do objeto
 
+    [SerializeField] private Animator animator;
+
     void Start()
     {
         currentScale = transform.localScale; // Inicializa a variável coma  escala que está no Inspector agora.
@@ -33,6 +35,8 @@ public class PlayerController : MonoBehaviour
     // NOVA FUNÇÃO PRA VIRAR O SPRITE NO ATAQUE
     void FlipSprite(string direction)
     {
+        // Dispara a animação de ataque
+        animator.SetTrigger("Attack");
         // Atualiza a escala baseada no que está agora no objeto
         currentScale = transform.localScale;
 
@@ -54,30 +58,20 @@ public class PlayerController : MonoBehaviour
         // Lança um raio na direção apertada
         RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, attackRange, enemyLayer);
 
-        // Debug visual para ver a linha na cena
         Debug.DrawRay(transform.position, direction * attackRange, Color.red, 0.5f);
 
-        if (hit.collider != null)
+        if (hit.collider != null && hit.collider.CompareTag("Enemy"))
         {
-            if (hit.collider.CompareTag("Enemy"))
-            {
-                // LÓGICA CORE:
-                // 1. Pega a posição do inimigo
-                Vector3 enemyPos = hit.collider.transform.position;
+            Vector3 enemyPos = hit.collider.transform.position;
+            Destroy(hit.collider.gameObject);
+            transform.position = enemyPos;
 
-                // 2. Destroi o inimigo
-                Destroy(hit.collider.gameObject);
-
-                // 3. Teleporta o player para a posição onde o inimigo estava
-                transform.position = enemyPos;
-
-                Debug.Log("Inimigo atingido!");
-            }
+            Debug.Log("Inimigo atingido!");
         }
         else
         {
-            // Opcional: Feedback de erro (ex: som de "miss")
             Debug.Log("Errou o golpe!");
         }
     }
+
 }
